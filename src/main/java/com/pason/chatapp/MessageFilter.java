7c16910d660e859203e779cbf710447f9e9c327b
ebabbe <Edward.Babbe@pason.com>
2014-06-10 16:54:30 -0500

    just added messfilter interface, motd will not deploy per app.js

---
+++ src/main/java/com/pason/chatapp/MessageFilter.java
@@ -0,0 +1,56 @@
+package com.pason.chatapp;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.ArrayUtils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+
+
+public interface MessageFilter
+{
+public String filterMessage(List badwords, String data);
+
+
+}
+
+class MessageFilterRegular implements MessageFilter {
+
+	@Override
+	public String filterMessage(List badwords, String data) {
+		
+    	
+    	
+    	data = data.replaceAll("\"", "");
+    	String [] words = data.split(" ");
+    	String newmessage = "";
+    		
+    		
+    	
+    	for(int x = 0; x < words.length; x++){
+    		String messageWord = words[x];
+    	if(badwords.contains(messageWord)){
+    		messageWord = "@$^&^@#";
+    		
+    	}
+    	newmessage += messageWord + " ";
+    }
+    	
+    	
+    	
+    	
+		return newmessage;
+    	
+    	
+   
+	}
+	
+}
