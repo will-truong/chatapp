522dee50435e234e12eff2344d4f6ff596b4a361
William Truong <William.Truong@pason.com>
2014-06-11 09:53:06 -0500

    works now

--- src/main/java/com/pason/chatapp/MessageFilter.java
+++ src/main/java/com/pason/chatapp/MessageFilter.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.ArrayUtils;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
