a629567a0eb75f5ee38e3c0b9cbb9ffb1fc1413c
William Truong <William.Truong@pason.com>
2014-06-11 14:01:02 -0500

    commented unit test

--- src/test/java/com/pason/chatapp/test/unit/MotDTest.java
+++ src/test/java/com/pason/chatapp/test/unit/MotDTest.java
@@ -14,26 +14,26 @@ public class MotDTest {
 	@Test
 	public void testWeatherRequest() {
 		MotdRegular vert1 = new MotdRegular();
-		assertEquals("Hi, no message of the day set.", vert1.getMotd());
-		vert1.setMotd("%name% %time% %cond% %temp%", "%name% %time%");
-		assertFalse(vert1.isWeatherReceived());
-		assertEquals("%name% %time%", vert1.getMotd());
-		HashMap<String, Object> updates = new HashMap<>();
-		updates.put("name", "will");
-		updates.put("time", "12p");
-		vert1.updateMotd(updates);
-		assertEquals("will 12p", vert1.getMotd());
-		updates = new HashMap<>();
-		updates.put("temp", 67);
-		updates.put("cond", "really foggy");
-		vert1.updateMotd(updates);
-		assertTrue(vert1.isWeatherReceived());
-		assertEquals("will 12p really foggy 67", vert1.getMotd());
-		updates = new HashMap<>();
-		updates.put("temp", null);
-		updates.put("cond", null);
-		vert1.updateMotd(updates);
-		assertFalse(vert1.isWeatherReceived());
-		assertEquals("will 12p", vert1.getMotd());
+		assertEquals("Hi, no message of the day set.", vert1.getMotd()); //this should be the motd by default
+		vert1.setMotd("1: %name% %time% %cond% %temp%", "2: %name% %time%"); //sets motd's with vars (1st motd w/ weather info, 2nd motd w/o weather info)
+		assertFalse(vert1.isWeatherReceived()); //weather info should not be present
+		assertEquals("2: %name% %time%", vert1.getMotd()); //motd w/o weather info should be sent
+		HashMap<String, Object> updates = new HashMap<>(); //prepare an update map
+		updates.put("name", "will"); //prepare to update "name" variable with "will"
+		updates.put("time", "12p"); //prepare to update "time" variable with "12p"
+		vert1.updateMotd(updates); //execute the update
+		assertEquals("2: will 12p", vert1.getMotd()); //motd with variables plugged in
+		updates = new HashMap<>(); //prepare a new update
+		updates.put("temp", 67); //prepare to update "temp" variable with 67
+		updates.put("cond", "really foggy"); //prepare to update "cond" variable with "really foggy"
+		vert1.updateMotd(updates); //execute the update
+		assertTrue(vert1.isWeatherReceived()); //weather info should be present
+		assertEquals("1: will 12p really foggy 67", vert1.getMotd()); //should retrieve the motd w/ weather info, variables replaced
+		updates = new HashMap<>(); //prepare a new update
+		updates.put("temp", null); //prepare to remove "temp" variable
+		updates.put("cond", null); //prepare to remove "cond" variable
+		vert1.updateMotd(updates); //execute the update
+		assertFalse(vert1.isWeatherReceived()); //weather info should not be present
+		assertEquals("2: will 12p", vert1.getMotd()); //motd w/o weather info should be executed
 	}
 }
